c77e011859267858660da7224e9fc282d16ad813
RikkiRu <rikoshetru@yandex.ru>
2015-10-10 11:13:21 +0300

    vg

--- project/project/Form1.cs
+++ project/project/Form1.cs
@@ -12,7 +12,7 @@ namespace project
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Its working good!");
+            MessageBox.Show("Its working very good!");
         }
     }
 }
